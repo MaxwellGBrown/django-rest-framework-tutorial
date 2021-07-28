018cd97615f73c2df94db6933990ad30621a4660
Maxwell G. Brown <maxwellgbrown@gmail.com>
2021-07-28 14:25:35 -0400

    Implement basic snippet infrastructure

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
 
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),  # noqa
+
+    path("snippets/", include("snippets.urls")),
 ]
