43eadd96dd165700c1cc8e8f7acea410045df2b0
Maxwell G. Brown <maxwellgbrown@gmail.com>
2021-07-28 13:50:39 -0400

    Quickstart tutorial

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """API Endpoints for viewing/editing Users."""
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """API Endpoints for viewing/editing Groups."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
